02f8314cfcc4afe56dbf5f2a4ca97953744836d5
RodrigOssaV <49349061+RodrigOssaV@users.noreply.github.com>
2021-02-11 15:32:20 -0300

    login-user
    npm i connect-flash
    view login

--- src/lib/passport.lib.js
+++ src/lib/passport.lib.js
@@ -5,6 +5,26 @@ const LocalStrategy = require('passport-local').Strategy;
 const pool = require('../database/database');
 const helpers = require('../lib/helpers.lib');
 
+// login
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async (req, username, password, done) => {
+    const rows = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
+    if(rows.length > 0){
+        const user = rows[0];
+        const validPassword = await helpers.matchPassword(password, user.password);
+        if (validPassword) {
+            done(null, user, req.flash('success', 'Welcome'));
+        } else {
+            done(null, false, req.flash('message', 'Incorrect password'));
+        }
+    } else {
+        done(null, false, req.flash('message', 'Username does not exists'));
+    }
+}));
+
 // singup
 passport.use('local.signup', new LocalStrategy({
     usernameField: 'username',
@@ -27,7 +47,7 @@ passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-passport.deserializeUser( async (id, done) => {
-    const rows = await pool.query('SELECT * FROM users WHERE id = ?', id);
+passport.deserializeUser(async (id, done) => {
+    const rows = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
     done(null, rows[0]);
 });
